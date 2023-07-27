71223de2863f8468a1895a3590c4f70bb7c940ee
PunpunB <20pbooncharoen@wakatipu.school.nz>
2023-07-28 09:16:44 +1200

    Sprint 1

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ $('#scroll-down').on('click', function() {
         2000
     );
 });
+
+let bosspick = document.getElementById('boss-name');
+bosspick.onchange = (ev) =>{
+console.log("Selected boss is: " + bosspick.value);
+}
+
+let classpick = document.getElementById('class-name');
+classpick.onchange = (ev) =>{
+console.log("Selected class is: " + classpick.value);
+}
+
+function changeimg()
+{
+    var img = document.getElementById('image');
+    var chosenclass = document.getElementById('classpick.value');
+    var chosenboss = document.getElementById('bosspick.value');
+    img.src = 'bg.jpg';
+    console.log(chosenclass);
+    console.log(chosenboss);
+} 
